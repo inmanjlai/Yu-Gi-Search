61e5a263af78dbf48992d9c78c3aa1a88540d68f
Ismail Manjlai <imanjlai@nhaschools.com>
2022-10-11 19:20:49 -0400

    fix incorrect indent

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import os
 
 # CONFIG ->
 app = Flask(__name__)
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace('postgres://', 'postgresql://')
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace('postgres://', 'postgresql://')
 app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 db = SQLAlchemy()
 
