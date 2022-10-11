7fccf9aadf982b0a4735407c264e8cdce6dd009f
Ismail Manjlai <imanjlai@nhaschools.com>
2022-10-11 19:19:06 -0400

    fix postgres to postgresql

--- app.py
+++ app.py
@@ -9,7 +9,8 @@ import os
 
 # CONFIG ->
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        'DATABASE_URL').replace('postgres://', 'postgresql://')
 app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 db = SQLAlchemy()
 
