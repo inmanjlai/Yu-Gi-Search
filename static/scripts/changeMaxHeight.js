0775b66eacf483741ce67101c4dc9a89288d42d3
ocahsa <inmanjlai@hotmail.com>
2023-03-22 17:16:17 -0400

    kill me

--- static/scripts/changeMaxHeight.js
+++ static/scripts/changeMaxHeight.js
@@ -3,22 +3,22 @@ const cardTypes = document.querySelectorAll('.card-types');
 
 function adjustMaxHeight() {
 
-    let aboveMidThreshold = (currentValue) => currentValue >= 10
-    let numberOfTypes = []
+    // let aboveMidThreshold = (currentValue) => currentValue >= 10
+    // let numberOfTypes = []
 
-    cardTypes.forEach((ele, idx) => {
-        if (idx !== 3) numberOfTypes.push(ele.children.length)
-    })
+    // cardTypes.forEach((ele, idx) => {
+    //     if (idx !== 3) numberOfTypes.push(ele.children.length)
+    // })
 
-    let count = 0
-    numberOfTypes.forEach((ele) => {
-        if (ele > 6) count+=1
-    })
+    // let count = 0
+    // numberOfTypes.forEach((ele) => {
+    //     if (ele > 6) count+=1
+    // })
 
-    if (count > 2) {
-        if (numberOfTypes.some(aboveMidThreshold) && window.innerWidth > 600) decklistContainer.style.maxHeight = "1500px"
-        else decklistContainer.style.maxHeight = "900px"
-    }
+    // if (count > 2) {
+    //     if (numberOfTypes.some(aboveMidThreshold) && window.innerWidth > 600) decklistContainer.style.maxHeight = "1500px"
+    //     else decklistContainer.style.maxHeight = "900px"
+    // }
 
 }
 
